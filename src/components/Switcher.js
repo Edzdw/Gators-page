fde2a6ac11a8db39504c7976ec2f39e55227f4c9
edzdw <minhtam09081999@gmail.com>
2023-09-28 14:14:25 +0700

    update layout

--- src/components/Switcher.js
+++ src/components/Switcher.js
@@ -11,7 +11,7 @@ const colors = [
   { id: 8, name: "goldenrod" },
 ];
 const Switcher = () => {
-  const [color, setColor] = useState("green");
+  const [color, setColor] = useState("yellow");
   const [toggle, setToggle] = useState(false);
 
   return (
